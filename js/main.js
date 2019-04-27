14f4102125f91bb230f86ffeadb14262c1921e3f
Diego Aquino <diegol.aquino@gmail.com>
2019-04-27 11:24:34 -0300

    Introdução do plugin do slide

--- js/main.js
+++ js/main.js
@@ -8,5 +8,5 @@ $(".rslides").responsiveSlides({
     auto: true,             // Boolean: Animate automatically, true or false
     speed: 500,            // Integer: Speed of the transition, in milliseconds
     timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
-    pager: false,           // Boolean: Show pager, true or false
+    pager: true,           // Boolean: Show pager, true or false
   });
